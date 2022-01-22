32ce6d7d1401c33264226ef5f93216485697e0a4
KrzysztofHum <k.humienny1996@protonmail.com>
2022-01-22 18:52:38 +0100

    create calculator

--- components/expenses/ExpenseCost.tsx
+++ components/expenses/ExpenseCost.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Button, Drawer, Grid } from "@mui/material";
+import { Button, Drawer, Grid, Typography } from "@mui/material";
 import styled from "styled-components";
 import { useExpenses } from "../../context/ExpensesProvider";
 
 const StyledGrid = styled(Grid)`
   min-height: 50vh;
+`;
+
+const StyledTypographyFooter = styled(Typography)`
+  background-color: #e6e4e4;
+  text-align: center;
 `;
 
 const ExpenseCost = () => {
@@ -22,18 +27,81 @@ const ExpenseCost = () => {
               <Button>Gotówka</Button>
             </Grid>
             <Grid item>
-              <Button>Zakupy</Button>
+              <Button>
+                <Grid>
+                  <span>To category</span>
+                  <Typography>foodstoofs</Typography>
+                </Grid>
+              </Button>
             </Grid>
           </Grid>
-          <Grid>
+          <Grid container direction="column" alignItems="center">
             <span>Wydatek</span>
-            <span>0 zł</span>
+            <Typography variant="h6">0 zł</Typography>
           </Grid>
-          <Grid>
+          <Grid container direction="column" alignItems="center">
             <span>Notatki...</span>
           </Grid>
-          <Grid>Kalkulator</Grid>
-          <Grid> Dzisiaj, 22 sty 2022</Grid>
+          <Grid container>
+            <Grid item xs={3}>
+              <Button>7</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>8</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>9</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>DELETE</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>4</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>5</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>6</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>DATA</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>1</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>2</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>3</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>zł</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>+</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>-</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>0</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>,</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>/</Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button>*</Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button>Confirm</Button>
+            </Grid>
+          </Grid>
+          <StyledTypographyFooter>Sobota, 22 sty 2022</StyledTypographyFooter>
         </StyledGrid>
       </Drawer>
     </>
