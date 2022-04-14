234ddab0a9b11fe9117a0e00f39a808b6dd8ba70
KrzysztofHum <k.humienny1996@protonmail.com>
2022-04-14 12:02:41 +0200

    styled user info component in HeaderMenuItems

--- components/header/HeaderMenuItems.tsx
+++ components/header/HeaderMenuItems.tsx
@@ -1,4 +1,4 @@
-import { Avatar, MenuItem, Stack } from "@mui/material";
+import { Avatar, Grid, MenuItem, Typography } from "@mui/material";
 import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
@@ -12,7 +12,14 @@ type Props = {
 };
 
 const StyledUserInfo = styled.div`
+  margin: 0;
   padding-top: 1rem;
+  background-color: ${(props) => props.theme.palette.primary.main};
+`;
+
+const StyledAvatar = styled(Avatar)`
+  background-color: white;
+  color: #acacac;
 `;
 
 const ListItem = ({ href, label, onClick, children }: Props) => (
@@ -40,20 +47,45 @@ const HeaderMenuItems = ({ handleCloseNavMenu }: any) => {
   return (
     <>
       <StyledUserInfo>
-        <div>
-          <Stack direction="row">
-            <Avatar>H</Avatar>
-          </Stack>
-        </div>
-        <div>{user?.displayName}</div>
+        <Grid container justifyContent="space-between" pl={2}>
+          <Grid item>
+            <StyledAvatar></StyledAvatar>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "flex", md: "flex" } }}
+            >
+              BudgetApp
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid pl={2} pb={1}>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: "flex", md: "flex" } }}
+          >
+            {user?.displayName}
+          </Typography>
+        </Grid>
       </StyledUserInfo>
-      <ListItem
-        href="/expenses"
-        label="My Expenses"
-        onClick={handleCloseNavMenu}
-      />
-      <ListItem href="/" label="My Investments" onClick={handleCloseNavMenu} />
-      <ListItem href="/" label="LOG OUT" onClick={handleSignOut} />
+      <Grid m={0}>
+        <ListItem
+          href="/expenses"
+          label="My Expenses"
+          onClick={handleCloseNavMenu}
+        />
+        <ListItem
+          href="/"
+          label="My Investments"
+          onClick={handleCloseNavMenu}
+        />
+        <ListItem href="/" label="LOG OUT" onClick={handleSignOut} />
+      </Grid>
     </>
   );
 };
